ecfe7bd00b65869b111fc0d1486d66f9ed3b89d1
Chris Vazquez <cvg_v10@hotmail.com>
2022-03-27 15:36:00 -0700

    added axios and api key and a res.data

--- Weather.Dash/java.js
+++ Weather.Dash/java.js
@@ -1,4 +1,8 @@
 document.getElementById('Search').addEventListener('click',event=>{
   let city = document.getElementById('City').value 
   console.log(city);
+  axios.get(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=3d312306696b0930e7fddd369a434ba3`)
+  .then(res =>{
+    console.log(res.data);
+  })
 })
