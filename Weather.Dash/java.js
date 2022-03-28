f09f78879cf6f42eb92b8568de75e6f218a27e83
Chris Vazquez <cvg_v10@hotmail.com>
2022-03-27 17:54:46 -0700

    added a response data in order to get the city to display and also added the long and lat to display the uv index

--- Weather.Dash/java.js
+++ Weather.Dash/java.js
@@ -3,6 +3,23 @@ document.getElementById('Search').addEventListener('click',event=>{
   console.log(city);
   axios.get(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=3d312306696b0930e7fddd369a434ba3`)
   .then(res =>{
-    console.log(res.data);
+    console.log(res.data)
+    let lat = res.data.city.coord.lat
+    let lon = res.data.city.coord.lon
+    axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&&appid=3d312306696b0930e7fddd369a434ba3`)
+    .then(resp=>{
+      console.log(resp.data);
+      document.getElementById('maindiv').innerHTML=`
+      <div class="card">
+        <div class="card-content">
+         <div class="content">
+            <div class="title">${res.data.city.name}
+              Lorem ipsum leo risus, porta ac consectetur ac, vestibulum at eros. Donec id elit non mi porta gravida at eget metus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras mattis consectetur purus sit amet fermentum.
+            </div>
+      </div>
+  </div>
+</div>
+      `
+    })
   })
 })
